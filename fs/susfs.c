04fb224966528e97061954d56d5873867990509d
hewel <hewel@vivaldi.net>
2026-07-14 20:32:49 +0800

    fix(susfs): support builds without optional features
    Signed-off-by: hewel <hewel@vivaldi.net>

--- fs/susfs.c
+++ fs/susfs.c
@@ -1189,7 +1189,7 @@ out_copy_to_user:
 }
 
 /* get susfs enabled features */
-static int copy_config_to_buf(const char *config_string, char *buf_ptr, size_t *copied_size, size_t bufsize) {
+static int __maybe_unused copy_config_to_buf(const char *config_string, char *buf_ptr, size_t *copied_size, size_t bufsize) {
 	size_t tmp_size = strlen(config_string);
 
 	*copied_size += tmp_size;
@@ -1204,7 +1204,7 @@ static int copy_config_to_buf(const char *config_string, char *buf_ptr, size_t *
 void susfs_get_enabled_features(void __user **user_info) {
 	struct st_susfs_enabled_features *info = (struct st_susfs_enabled_features *)kzalloc(sizeof(struct st_susfs_enabled_features), GFP_KERNEL);
 	char *buf_ptr = NULL;
-	size_t copied_size = 0;
+	size_t copied_size __maybe_unused = 0;
 
 	if (!info) {
 		info->err = -ENOMEM;
